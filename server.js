397b074e3a3d5738e0a2dea0c73b2c9c0e3f7a14
Jordhan Carvalho <jordhan.rdz@gmail.com>
2019-06-04 15:17:52 -0300

    Setup initial Routes

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const app = express();
 // Connect DB
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/rents', require('./routes/api/rents'));
 
 const PORT = process.env.PORT || 5000;
 
