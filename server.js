14313f41f452326b9ebb7d614f023b0e7e158c8c
Jordhan Carvalho <jordhan.rdz@gmail.com>
2019-06-12 10:24:35 -0300

    Missed semi-column

--- server.js
+++ server.js
@@ -18,12 +18,13 @@ app.use('/api/rents', require('./routes/api/rents'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
