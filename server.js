56a5e7a7d0e9814672cb775f6049db98abcef599
Jordhan Carvalho <jordhan.rdz@gmail.com>
2019-06-12 10:13:09 -0300

    Deploy to heroku configs

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./db');
 
 const app = express();
@@ -14,6 +15,15 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/rents', require('./routes/api/rents'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+     app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 
 const PORT = process.env.PORT || 5000;
 
